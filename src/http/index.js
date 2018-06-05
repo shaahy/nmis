451d03f7e366e1ef523958029748b19485143c20
shaahy <2313496750@qq.com>
2018-06-05 10:48:48 +0800

    http多实例配置

--- src/http/index.js
+++ src/http/index.js
@@ -4,10 +4,13 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: "http://localhost:3000"
 })
-
+//配置二
 const instance2 = axios.create({
   baseURL: "http://localhost:4000"
 })
 export default instance
 export {instance2}
 
+
+//在需要使用的时导入不同的axios实例
+//import { instance, instance2} from './*/http'
